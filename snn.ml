3f0d78bafc3d62fe6e6ce0a3dfa2abb7bdfc2144
dhruv <dhruv.sharma@iiitb.org>
2022-05-09 09:12:04 +0530

    Comments

--- snn.ml
+++ snn.ml
@@ -17,13 +17,15 @@ Some other functions used are -
 open Printf                       
 type 'a io       = { i: 'a; o: 'a }                                  
 type vec         = float array          
-type 2Dvec         = vec array              
-type neuralNet   = { a : vec io; ah : vec; w : 2Dvec io; c : 2Dvec io }          
-let vector       = Array.init   
+type mat         = vec array              
+type neuralNet   = { a : vec io; ah : vec; w : mat io; c : mat io }          
+let  vector       = Array.init   
 
 
+(* Create a 2Darray of dimensions m*n , element of array is returned by function  *)
 let matrix m n f = vector m (fun i -> vector n (f i))                        
 
+(* Create a neural net with ni -> number of input neurons ; nh->number of hidden neurons ;no-> number of output neurons *)
 let neuralNet ni nh no =                  
     let init fi fo = { i = matrix (ni + 1) nh fi; o = matrix nh no fo } in   
     let rand x0 x1 = x0 +. Random.float(x1 -. x0) in                         
@@ -58,16 +60,6 @@ let forward net x =
     let ao k = sigmoid(dot nh (Array.get ah) (fun j -> net.w.o.(j).(k))) in        
     {net with a = { i = vector ni ai; o = vector no ao }; ah = ah }          
 
-
-let rec train net inputs iters n m =    
-    if iters = 0 then net else            
-        let step (net, err) (x,y) =                            
-            let net, de = backward (forward net x) y n m in   
-            net, err +. de in           
-        let net, err = Array.fold_left step (net, 0.0) inputs in         
-        if iters mod 10000 = 0 then printf "Error: %g:\n%!" err;           
-        train net inputs (iters - 1) n m
-
 let update net inputs =                   
     let ni, nh, no = length net.a.i, length net.ah, length net.a.o in        
     assert(length inputs = ni-1);         
@@ -77,6 +69,7 @@ let update net inputs =
     let ao k = sigmoid(dot nh (get ah) (fun j -> net.w.o.(j).(k))) in        
     {net with a = { i = vector ni ai; o = vector no ao }; ah = ah }          
 
+        
 let backPropagate net targets n m =       
     let ni, nh, no = length net.a.i, length net.ah, length net.a.o in        
 
@@ -91,10 +84,20 @@ let backPropagate net targets n m =
     let ci i j = hd.(j) *. net.a.i.(i) in 
     let wi i j = net.w.i.(i).(j) +. n *. ci i j +. m *. net.c.i.(i).(j) in   
 
-    let init fi fo = { i = matrix ni nh fi; o = matrix nh no fo } in         
-    { net with w = init wi wo; c = init ci co },                             
-    0.5 *. fold2 no (fun t x y -> t +. (x -. y) ** 2.0) 0.0                  
-                (get targets) (get net.a.o) 
+let init fi fo = { i = matrix ni nh fi; o = matrix nh no fo } in         
+{ net with w = init wi wo; c = init ci co },                             
+0.5 *. fold2 no (fun t x y -> t +. (x -. y) ** 2.0) 0.0                  
+            (get targets) (get net.a.o) 
+    
+    
+let rec train net patterns iters n m =    
+    if iters = 0 then net else            
+        let step (net, error) (inputs, targets) =                            
+            let net, de = backPropagate (update net inputs) targets n m in   
+            net, error +. de in           
+        let net, error = Array.fold_left step (net, 0.0) patterns in         
+        if iters mod 10000 = 0 then printf "Error: %g:\n%!" error;           
+        train net patterns (iters - 1) n m        
 
 
 let print_array ff print xs =             
